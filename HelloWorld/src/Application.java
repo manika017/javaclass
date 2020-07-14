996d8bf304d099da3516c57482f5acf18f2bfc6a
Manika Nunna <manikanunna@Manikas-Air.home>
2020-07-13 19:18:06 -0700

    hello world project

---
+++ HelloWorld/src/Application.java
@@ -0,0 +1,27 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("HelloWorld!");
+		
+		//declaring variables 
+		int x;
+		x=11; 
+		
+		//int x;
+		//second: you cannot declare same variable again
+		
+		int y =12;
+		int z;
+		
+		z = x+y;
+		
+		String name = "Manika Nunna";
+		
+		System.out.println(z);
+		System.out.println(name);
+	}
+
+}
