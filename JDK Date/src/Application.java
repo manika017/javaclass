d5a524b7f0c51098589a928d3428e23efc6d7d0d
manika017 <mnunna09@gmail.com>
2020-08-27 22:12:45 -0700

    eclipse

---
+++ JDK Date/src/Application.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Date date;
+		java.util.Date date2;
+		date = new java.util.Date();
+		System.out.println("today's date is " + date.toString());
+
+	}
+
+}
