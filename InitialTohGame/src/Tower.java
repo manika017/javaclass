bf377d0535fd5af87b42f4df6a0e25cc615cfa15
manika017 <mnunna09@gmail.com>
2020-08-27 21:24:04 -0700

    eclipse

---
+++ InitialTohGame/src/Tower.java
@@ -0,0 +1,26 @@
+
+public class Tower {
+
+	String name;
+
+	void draw() {
+		drawPole();
+		drawBase();
+		drawTitle();
+	}
+
+	void drawPole() {
+		for (int i = 0; i < 4; i++) {
+			System.out.println("     |");
+		}
+	}
+
+	void drawBase() {
+		System.out.println("-----------");
+	}
+
+	void drawTitle() {
+		System.out.println("  " + name);
+	}
+
+}
